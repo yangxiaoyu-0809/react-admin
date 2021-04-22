e35f57346b1e1ca0eed9d6102e3b8609504c299b
yangxiaoyu <yang_xiaoyu0809@163.com>
2021-04-22 17:52:18 +0800

    组织管理页面demo搭建

--- src/pages/OrgMg/index.tsx
+++ src/pages/OrgMg/index.tsx
@@ -1,10 +1,15 @@
-import { Button, message, Input, Drawer } from 'antd';
+import { Row,Col } from 'antd';
 import React, { useState, useRef } from 'react';
+import Style from './tree.less'
 
 const OrgMg: React.FC = () => {
-
     return (
-       <div>组织管理页面</div>
+        <>
+            <Row>
+                <Col className={Style.leftCon} span={9}>左侧</Col>
+                <Col className={Style.rightCon} span={15}>右侧</Col>
+            </Row>
+        </>
     );
 };
 
