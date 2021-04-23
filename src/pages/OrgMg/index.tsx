84177876e70f8799905e15666ab779ce73a0e843
yangxiaoyu <yang_xiaoyu0809@163.com>
2021-04-23 18:06:19 +0800

    up

--- src/pages/OrgMg/index.tsx
+++ src/pages/OrgMg/index.tsx
@@ -1,5 +1,6 @@
 import { Row,Col } from 'antd';
 import React, { useState, useRef } from 'react';
+import Tree from './tree';
 import Style from './tree.less'
 
 const OrgMg: React.FC = () => {
@@ -7,12 +8,9 @@ const OrgMg: React.FC = () => {
         <>
             <Row>
                 <Col className={Style.leftCon} span={10}>
-                    
+                    <Tree>
 
-                    左侧
-
-
-
+                    </Tree>
                 </Col>
                 <Col className={Style.rightCon} span={14}>右侧</Col>
             </Row>
---
+++ src/pages/OrgMg/tree.tsx
@@ -0,0 +1,14 @@
+import { Row,Col } from 'antd';
+import React, { useState, useRef } from 'react';
+
+import Style from './tree.less'
+
+const Tree: React.FC = () => {
+    return (
+        <>
+            这是树页面
+        </>
+    );
+};
+
+export default Tree;
