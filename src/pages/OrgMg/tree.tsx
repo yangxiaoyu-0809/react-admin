c07e5afbb836b50717f262ca9715755f03518d11
yangxiaoyu <1150055925@qq.com>
2021-04-24 22:03:15 +0800

    up

--- src/pages/OrgMg/tree.tsx
+++ src/pages/OrgMg/tree.tsx
@@ -1,12 +1,34 @@
-import { Row,Col } from 'antd';
+import {Button, Input, Switch} from 'antd';
+import { CloseOutlined, CheckOutlined } from '@ant-design/icons';
 import React, { useState, useRef } from 'react';
 
 import Style from './tree.less'
 
+const { Search } = Input;
+
+const onSearch = value => console.log(value);
 const Tree: React.FC = () => {
     return (
         <>
-            这是树页面
+            <span>显示被注销的组织</span>
+            <Switch
+                checkedChildren={<CheckOutlined/>}
+                unCheckedChildren={<CloseOutlined/>}
+                defaultChecked
+            />
+            <span>隐藏被注销的组织</span>
+
+            <span>|</span>
+            <Button type="primary">展开所有组织</Button>
+
+            <Search
+                placeholder="input search text"
+                allowClear
+                enterButton="查询"
+                size="large"
+                onSearch={onSearch}
+            />
+
         </>
     );
 };
